7eccfde36d849ade130b158d251a9a8510cb3052
MichaelBirkholz <michael.birkholz@student.uni-siegen.de>
2019-09-09 16:47:19 +0200

    added Style Class

---
+++ Style.java
@@ -0,0 +1,36 @@
+package com.yoururcap.impl;
+
+import java.awt.*;
+
+public class Style {
+	public static final int HORIZONTAL_SPACING = 10;
+	public static final int VERTICAL_SPACING = 10;
+	public static final int LARGE_VERTICAL_SPACING = 25;
+	public static final int XLARGE_VERTICAL_SPACING = 50;
+	public static final int FONT_SMALL = 10;
+	public static final int FONT_DEFAULT = 12;
+	public static final int FONT_MEDIUM = 13;
+	public static final int FONT_LARGE = 14;
+	public static final int FONT_VERY_LARGE = 16;
+	public static final int FONT_MAIN_HEADING_SIZE = 24;
+	public static final int FONT_HUGE = 36;
+	public static final int BORDER_THIN = 1;
+	public static final int BORDER_NORMAL = 2;
+	public static final int BORDER_WIDE = 5;
+	public static final int HEIGHT_TINY = 15;
+	public static final int HEIGHT_VERY_SMALL = 25;
+	public static final int HEIGHT_SMALL = 30;
+	public static final int HEIGHT_DEFAULT = 32;
+	public static final int HEIGHT_MEDIUM = 40;
+	public static final int HEIGHT_LARGE = 50;
+	public static final int HEIGHT_VERY_LARGE = 75;
+	public static final int HEIGHT_BIG = 85;
+	public static final int HEIGHT_HUGE = 160;
+	public static final int MAIN_PANE_WIDTH = 660;
+	public static final int MAIN_PANE_HEIGTH = 617;
+	public static final int MAIN_BOTTOM_DEADSPACE = 17;
+	public static final Dimension INPUTFIELD_SIZE_DEFAULT = new Dimension(50, HEIGHT_DEFAULT);
+	public static final Dimension INPUTFIELD_SIZE_LARGE = new Dimension(106,HEIGHT_DEFAULT);
+	public static final Dimension COMBOBOX_SIZE = new Dimension(150,HEIGHT_DEFAULT);
+
+}
